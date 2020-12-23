0981894b0665fdf020c2b2055b8656505aa47372
luciheit@gmail.com <luciheit@gmail.com>
2020-12-23 15:10:57 -0300

    listas de mkp, categorias e subcategorias e menu de categorias

--- 20.12.22/marketplaces/main.py
+++ 20.12.22/marketplaces/main.py
@@ -1,12 +1,18 @@
-from marketplaces import Marketplace, Category
+from marketplaces import Marketplace, Category, Subcategory
 
-def menu():
-    options = ['Submarino', 'Via Varejo', 'B2W', 'Zoom', 'Sair']
+mktplaces = [Marketplace('Amazon'), Marketplace('B2W'), Marketplace('Zoom'), Marketplace('Carrefour'), Marketplace('Sair')]
+categorias = [Category('Móveis', mktplaces[1]), Category('Telefonia', mktplaces[0]), Category('Eletrodomésticos', mktplaces[1])]
+subcategorias = [Subcategory('Sofá', categorias[0]), Subcategory('Mesa', categorias[0])]
+
+def menu(): 
 
     print('\nMENU: ')
 
-    for i, option in enumerate(options):
-        print(f'[{i+1}] - {option}')
+    i = 1
+
+    for option in mktplaces:
+        print(f'{i} - {option}')
+        i += 1
 
     op = int(input('Selecione uma opção: '))
     return op
@@ -15,20 +21,39 @@ while True:
     try:
         op = menu()
         if op == 1:
-            print(op)
-            resultado = Category.categories.keys()
+            print(f'\nVocê escolheu a opção {mktplaces[0]}\n')
+            for i in categorias:
+                if i.get_parentname() ==  mktplaces[0].get_name():
+                    print(i.get_name())
+                    
         elif op == 2:
-            resultado = sub(valor1, valor2)
+            item = 1
+            print(f'\nVocê escolheu a opção {mktplaces[1]}\n')
+            for i in categorias:
+                if i.get_parentname() ==  mktplaces[1].get_name():
+                    print(f'{item} - {i.get_name()}')
+                    item += 1
+            escolher_cat = int(input(f'Digite uma opção de categoria: '))
+            return escolher_cat
+
         elif op == 3:
-            resultado = multi(valor1, valor2)
+            print(f'\nVocê escolheu a opção {mktplaces[2]}\n')
+            for i in categorias:
+                if i.get_parentname() ==  mktplaces[2].get_name():
+                    print(i.get_name())
+
         elif op == 4:
-            resultado = divi(valor1, valor2)
+            print(f'\nVocê escolheu a opção {mktplaces[3]}\n')
+            for i in categorias:
+                if i.get_parentname() ==  mktplaces[3].get_name():
+                    print(i.get_name())
+            
         elif op == 5:
             exit(0) #tentar alterar
         else:
             print('Digite uma opção válida!')
-        print(f'Agora escolha uma categoria: {resultado}.')
+        #print(f'Agora escolha uma categoria: {resultado}.')
+        break
         
     except ValueError:
         print('Opção indisponível. Tente novamente.')
-    
--- 20.12.22/marketplaces/marketplaces.py
+++ 20.12.22/marketplaces/marketplaces.py
@@ -1,19 +1,57 @@
 class Marketplace:
-    marketplace = [
-        'B2W',
-        'Amazon',
-        'Mercado Livre'
-    ]
+    def __init__(self, Marketplace: str):
+        self.__namemkp = Marketplace
+
+    def get_name(self) -> str:
+        return self.__namemkp
+
+    def __str__(self) -> str:
+        return f'''{self.__namemkp}'''
+
 
 class Category:
-    categories = {
-        'Mobile': ['Celulares', 'Tablets'],
-        'Móveis': ['Mesa', 'Guarda-Roupa'],
-        'Eletrônicos': ['TV', 'Rádio']
-    }
+    def __init__(self, Category: str, Marketplace: Marketplace):
+       self.__catmkp = Category
+       self.__parent = Marketplace
+
+    def get_name(self) -> str:
+        return self.__catmkp
+
+    def get_parentname(self) -> str:
+        return self.__parent.get_name()
+
+
+class Subcategory:
+    def __init__(self, Subcategory: str, Category: Category):
+        self.__subcat = Subcategory
+        self.__parent = Category
+
+    def get_subcat(self) -> str:
+        return self.__subcat
+    
+    def get_parentname(self) -> str:
+        return self.__parent.get_name()
+    
 
 #def categorias():
 #    print('Categorias')
 #
 #    for j in categorias:
 #        print(j)
+
+# class Category:
+#     categories = [ 'Moveis', 'Eletrônicos', 'Mobile']
+#         # {'MOVEIS': 'Móveis',
+#         # 'ELETRÔNICOS': 'Eletrônicos',
+#         # 'MOBILE': 'Mobile'
+#     # }
+
+# class Subcategory(Category):
+#     subcategories = {
+#         'MESA': 'Mesa',
+#         'ESCRIVANINHA': 'Escrivaninha',
+#         'TV': 'TV',
+#         'COMPUTADORES': 'Computadores',
+#         'CELULARES': 'Celulares',
+#         'TABLET': 'Tablet'
+#     }
