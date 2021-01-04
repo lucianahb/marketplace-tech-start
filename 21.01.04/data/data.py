a491b41e61079ad86dc8932cf0a43968e0125781
luciheit@gmail.com <luciheit@gmail.com>
2021-01-04 16:20:18 -0300

    Conserto do caminho dos arquivos

--- 21.01.04/data/data.py
+++ 21.01.04/data/data.py
@@ -1,12 +1,13 @@
 def write_mkp(marketplace, description):  
-    arq = open('marketplace.txt', 'a')
+    arq = open('21.01.04/data/marketplace.txt', 'a')
     string_mtp = f'{marketplace};{description}\n'
     arq.write(string_mtp)
     arq.close()
     
 def write_prod(product, description, price):
-    arq = open('product.txt', 'a')
+    arq = open('21.01.04/data/product.txt', 'a')
     string_prod = f'{product};{description};{price}\n'
+    arq.write(string_prod)
     arq.close()
 
 def save_mkp(marketplace, description):
