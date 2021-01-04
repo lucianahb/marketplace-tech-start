c46711e083423fc21036a1aaa63ac0ff44983375
luciheit@gmail.com <luciheit@gmail.com>
2021-01-04 15:36:30 -0300

    Criadas rotas de adição de marketplace

--- 21.01.04/web.py
+++ 21.01.04/web.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+import sys
+from flask import Flask, render_template, request
+
+sys.path.append('21.01.04/data')
+
+from data import save_mkp, save_prod
 
 app = Flask(__name__)
 
@@ -8,6 +13,13 @@ def index():
 
 @app.route('/createmkp')
 def createmkp():
-    return render_template('creatmtp.html')
+    return render_template('createmkp.html')
+
+@app.route('/marketplace')
+def savemkp():
+    name = request.args.get('name')
+    description = request.args.get('description')
+    save_mkp(name, description)
+    return '<h1> Marketplace salvo! </h1>'
 
 app.run(debug=True)
