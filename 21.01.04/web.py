19aebaeaf26fedcc6beea6483c48f78f7edb4a0c
luciheit@gmail.com <luciheit@gmail.com>
2021-01-04 16:20:37 -0300

    Criada rota de produtos

--- 21.01.04/web.py
+++ 21.01.04/web.py
@@ -20,6 +20,19 @@ def savemkp():
     name = request.args.get('name')
     description = request.args.get('description')
     save_mkp(name, description)
-    return '<h1> Marketplace salvo! </h1>'
+    return '<h1> Marketplace saved! </h1>'
+
+@app.route('/createprod')
+def createprd():
+    return render_template('createprod.html')
+
+@app.route('/product')
+def saveprod():
+    name = request.args.get('name')
+    description = request.args.get('description')
+    price = request.args.get('price')
+    save_prod(name, description, price)
+    return '<h1> Product saved! </h1>'
+
 
 app.run(debug=True)
