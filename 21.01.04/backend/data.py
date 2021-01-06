d41293e3c19c9634ec694508ee3ac2d94deb29ad
jcosta86 <jeferson.costa@olist.com>
2021-01-06 11:16:42 -0300

    adicionando read_historic ao log

--- 21.01.04/backend/data.py
+++ 21.01.04/backend/data.py
@@ -25,17 +25,32 @@ def save_mkp(marketplace, description):
     )
 
 
-def save_prod(product, description, price):
-    arq = open('21.01.04/backend/product.txt', 'a')
+def save_prod(product: str, description: str, price: str) -> None:
+    """Save a product in the products.txt file and record this action in the log.
+
+    Args:
+        product (str): Product name
+        description (str): Product description
+        price (str): Product price
+    """
+    file_ = open('21.01.04/backend/product.txt', 'a')
     string_prod = f'{{"name": "{product}", "description": "{description}", "price": "{price}"}}\n'
-    arq.write(string_prod)
-    arq.close()
+    file_.write(string_prod)
+    file_.close()
     write_log(
         f'Saved Product {product} with description {description} and price {price}'
     )
     
     
 def read_historic(path_file: str) -> list:
+    """Reads the data file and returns it as a list
+
+    Args:
+        path_file (str): File path. Example: 'products / products_list.txt'
+
+    Returns:
+        list: List containing the lines of the read file
+    """
     file_lines = []
     file_ = open(path_file, 'r')
     for line_in_file in file_:
@@ -43,13 +58,5 @@ def read_historic(path_file: str) -> list:
         json_line = json.loads(line)
         file_lines.append(json_line)
     file_.close()
+    write_log(f'Read historic in {path_file:}')
     return file_lines
-
-
-def read_products(products_list: list):
-    pass
-
-dic = read_historic('21.01.04/backend/product.txt')
-print(dic)
-for l in dic:
-    print(l['name'])
