3e8c7a9f84a2bfef49c4083dec4f1062724653cd
jcosta86 <jeferson.costa@olist.com>
2021-01-05 21:11:57 -0300

    alterando tipo de registro para padrão json

--- 21.01.04/backend/data.py
+++ 21.01.04/backend/data.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 
@@ -7,12 +8,6 @@ def write_mkp(marketplace, description):
     arq.write(string_mtp)
     arq.close()
 
-
-def write_prod(product, description, price):
-    arq = open('21.01.04/backend/product.txt', 'a')
-    string_prod = f'{product};{description};{price}\n'
-    arq.write(string_prod)
-    arq.close()
 
 
 def write_log(log):
@@ -31,7 +26,30 @@ def save_mkp(marketplace, description):
 
 
 def save_prod(product, description, price):
-    write_prod(product, description, price)
+    arq = open('21.01.04/backend/product.txt', 'a')
+    string_prod = f'{{"name": "{product}", "description": "{description}", "price": "{price}"}}\n'
+    arq.write(string_prod)
+    arq.close()
     write_log(
         f'Saved Product {product} with description {description} and price {price}'
     )
+    
+    
+def read_historic(path_file: str) -> list:
+    file_lines = []
+    file_ = open(path_file, 'r')
+    for line_in_file in file_:
+        line = line_in_file.strip()
+        json_line = json.loads(line)
+        file_lines.append(json_line)
+    file_.close()
+    return file_lines
+
+
+def read_products(products_list: list):
+    pass
+
+dic = read_historic('21.01.04/backend/product.txt')
+print(dic)
+for l in dic:
+    print(l['name'])
