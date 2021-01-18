e4a1a313ce4f70d5c6001ea232c6274e69ca5436
Gustavo Sobreiro de Assis <gustavo.sobreiro@olist.com>
2021-01-18 13:56:40 -0300

    [Backend] Colocando property e setter nas classes Seller e Marketplace.

--- backend/models/seller.py
+++ backend/models/seller.py
@@ -1,7 +1,42 @@
 class Seller:
     
     def __init__(self,name: str, tel: str, email: str, id:int = None) -> None:
-        self.id = id
-        self.name= name
-        self.tel=tel
-        self.email=email
+        self.__id = id
+        self.__name= name
+        self.__tel=tel
+        self.__email=email
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def tel(self):
+        return self.__tel
+
+    @property
+    def email(self):
+        return self.__email
+
+    @id.setter
+    def id(self,id):
+        self.__id=id
+
+    @name.setter
+    def name(self,name):
+        self.__name=name
+
+    @tel.setter
+    def tel(self,tel):
+        self.__tel=tel
+
+    @email.setter
+    def email(self,email):
+        self.__email=email
+
+    def __str__(self):
+       return str(id(self))
